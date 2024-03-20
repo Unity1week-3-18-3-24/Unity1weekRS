4cd5e402e7844731c02404a1d094100277602a41
うしょう <usho_feat_fubuki@yahoo.co.jp>
2024-03-20 11:26:44 +0900

    Create BarrierScript.cs

---
+++ Assets/Scripts/barrier/BarrierScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵のダメージ処理用のスクリプト
+/// Damageはinspectorで変えられるようにしてあります
+/// </summary>
+public class BarrierScript : MonoBehaviour
+{
+    [SerializeField] int Damage; //Playerに与えるダメージ
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    //PlayerのDamege関数を実行させる
+    {
+        if(col.gameObject.GetComponent<PlayerController>())
+        //当たったオブジェクトがPlayerControllerのコンポーネントがあった時に実行
+        //ダメージ与えたら消えます
+        {
+            col.gameObject.GetComponent<PlayerController>().PlayerDamage(Damage); //引数にDamageを入れてPlayerDamageを実行
+            Destroy(this.gameObject);
+        }
+    }
+}
